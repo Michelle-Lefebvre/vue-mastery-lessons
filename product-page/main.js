c282f52cc10f8efcc78fabe45d43c3cdecd932e4
Michelle <michelle.lefebvre@valtech.com>
2021-12-13 15:23:31 -0500

    conditional & list rendering

--- product-page/main.js
+++ product-page/main.js
@@ -3,6 +3,20 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        inventory: 5,
+        onSale: true,
+        details: ["80% cotton", "2% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
+        sizes: ["xs", "s", "m", "l", "xl", "xxl"]
     }
 })
