4ce3c44730fff4eb0b7b96ae8ea22d558477167e
Michelle <michelle.lefebvre@valtech.com>
2021-12-13 16:40:01 -0500

    Event handling & class style binding

--- product-page/main.js
+++ product-page/main.js
@@ -4,19 +4,34 @@ var app = new Vue({
         product: 'Socks',
         image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inventory: 5,
+        inventory:10,
         onSale: true,
         details: ["80% cotton", "2% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
             },
             {
                 variantId: 2235,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+
             }
         ],
-        sizes: ["xs", "s", "m", "l", "xl", "xxl"]
-    }
+        sizes: ["xs", "s", "m", "l", "xl", "xxl"],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart++
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart() {
+            this.cart--
+        }
+    },
 })
