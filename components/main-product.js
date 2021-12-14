90e077073dffc8f03dd974ef11a9f67f33e7093e
Michelle <michelle.lefebvre@valtech.com>
2021-12-14 14:37:11 -0500

    communiate events to parent

--- components/main-product.js
+++ components/main-product.js
@@ -8,14 +8,14 @@ Vue.component('product-details', {
     },
     template: `
     <div>
-          <p><u>Materials:</u></p>
-          <ul>
-              <li v-for="detail in details">{{ detail }}</li>
-            </ul>
-        </div>`
+        <p><u>Materials:</u></p>
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+    </div>`
 })
 
-Vue.component('product',  {
+Vue.component('product', {
     props: {
         premium: {
             type: Boolean,
@@ -67,9 +67,6 @@ Vue.component('product',  {
             :class="{ disabledButton: !inStock }"
             >Remove From Cart</button>
 
-            <div class="cart">
-                <p><small>Cart:</small> {{cart}}</p>
-            </div>
         </div>
         <hr>
         <product-details :details="details"></product-details>
@@ -78,44 +75,40 @@ Vue.component('product',  {
   </div>
     `,
     data() {
-
-
-    return {
-        brand: 'Vue Mastery',
-        product: 'Socks',
-        selectedVariant: 0,
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        details: ["80% cotton", "2% polyester", "Gender-neutral"],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "green",
-                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-                variantQuantity: 100,
-                variantSale: true
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue",
-                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
-                variantQuantity: 6,
-                variantSale: false
-            }
-        ],
-        sizes: ["XS", "S", "M", "L", "XL", "XXL"],
-        cart: 0,
-        onSale: true
-    }
-},
+        return {
+            brand: 'Vue Mastery',
+            product: 'Socks',
+            selectedVariant: 0,
+            link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+            details: ["80% cotton", "2% polyester", "Gender-neutral"],
+            variants: [{
+                    variantId: 2234,
+                    variantColor: "green",
+                    variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 100,
+                    variantSale: true
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "blue",
+                    variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 6,
+                    variantSale: false
+                }
+            ],
+            sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+            onSale: true
+        }
+    },
     methods: {
-        addToCart: function() {
-            this.cart += 1
+        addToCart() {
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
         },
-        updateProduct: function(index) {
+        updateProduct: function (index) {
             this.selectedVariant = index
         },
         removeFromCart() {
-            this.cart--
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
         }
     },
     computed: {
@@ -129,7 +122,7 @@ Vue.component('product',  {
             return this.variants[this.selectedVariant].variantQuantity
         },
         sale() {
-            if(this.variants[this.selectedVariant].variantSale === true) {
+            if (this.variants[this.selectedVariant].variantSale === true) {
                 return this.brand + ' ' + this.product + ' are on sale!'
             }
             return this.brand + ' ' + this.product + ' are not on sale!'
@@ -146,6 +139,20 @@ Vue.component('product',  {
 var app = new Vue({
     el: '#app',
     data: {
-        premium: true
+        premium: true,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+
+        },
+        removeFromCart(id) {
+            for(var i = this.cart.length - 1; i >= 0; i--) {
+                if (this.cart[i] === id) {
+                   this.cart.splice(i, 1);
+                }
+            }
+        }
     }
 })
